ed8a7629cde565583d70d9fd482205b7c2e988cd
andrewh323 <102182355+andrewh323@users.noreply.github.com>
2024-06-14 19:44:16 -0400

    Create BubbleSort.java

---
+++ measuring-sorts/BubbleSort.java
@@ -0,0 +1,25 @@
+package measuringSorts
+
+public class BubbleSort extends Instrumentation {
+	
+	static void bubbleSort(int arr[]) {
+		int i, j;
+		int n = arr.length;
+		for (i=0; i < n-1; i++) {
+			for (j=0; j < n-i-1; j++) {
+				if (arr[j] > arr[j+1]) {
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		ins.activate(true);
+		ins.startTiming("BubbleSort");
+		bubbleSort(TestClass.populateArray());
+		ins.stopTiming("BubbleSort");
+	}
+}
